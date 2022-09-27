094cd7bb053ff7787a606622cf7366b4d9d3e761
Henspa <81181977+Henspa@users.noreply.github.com>
2022-09-27 19:58:54 +0300

    Add files via upload

--- App.js
+++ App.js
@@ -1,11 +1,72 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { NavigationContainer } from '@react-navigation/native';
+import { AntDesign } from '@expo/vector-icons';
+import { Button, StyleSheet, Text, View } from 'react-native';
+import { useLayoutEffect } from 'react';
+import { useState } from 'react';
 
 export default function App() {
+
+  const Stack = createNativeStackNavigator();
+
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen
+          name= 'Home'
+          component={HomeScreen}
+          options={{
+            title: 'My home',
+            headerTitle: 'My sweet home'
+          }}
+        />
+        <Stack.Screen
+          name= 'Profile'
+          component={ProfileScreen}
+          options={{
+            title: 'Second',
+            headerTitle: 'Second'
+          }}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
+
+const HomeScreen = ({navigation}) => {
+
+  const [message, useMessage] = useState('Testing');
+
+  useLayoutEffect(() => {
+    navigation.setOptions({
+      headerStyle: {
+        backgroundColor: '#f0f0f0'
+      },
+      headerRight: () => (
+        <AntDesign
+        style={styles.navButton}
+        name="arrowright"
+        size={24}
+        onPress= {() => navigation.navigate('Second', {message: message})}
+        />
+      )
+    })
+  }, [message])
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text>Home Screen</Text>
+      <Button title='Profile' onPress={() => navigation.navigate('Profile') }></Button>
+    </View>
+  );
+}
+
+const ProfileScreen = ({navigation}) => {
+  return (
+    <View>
+      <Text>Profile Screen</Text>
+    
     </View>
   );
 }
@@ -17,4 +78,8 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  navButton: {
+    marginRight: 5,
+    padding: 4,
+  }
 });
